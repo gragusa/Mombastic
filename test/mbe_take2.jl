8a8dcbee6a7714556d628505f8a9d548352467ad
Giuseppe Ragusa <giuseppe.ragusa@gmail.com>
2018-10-06 19:01:58 +0200

    Updated

--- test/mbe_take2.jl
+++ test/mbe_take2.jl
@@ -1,53 +1,46 @@
+using Mombastic
 using Test
 using Random
+using LinearAlgebra
 
 Random.seed!(1)
-n, m, p = 1000, 20, 8
-y = randn(1000,1);
-x = randn(1000,p);
-z = randn(1000,m);
+n, m, p = 1000, 6, 4
+y = randn(n,1);
+x = randn(n,p);
+z = randn(n,m);
 gi_val = Array{Float64}(undef, n, m);
-
 
 function gi!(gi_val, x0)
     gi_val .= z.*(y .- x*x0)
     return
 end
 
-
-
-x0 = fill(0,p)
-
+x0 = fill(0.,p)
 mf = Mombastic.OnceDiffMoments(gi!, gi_val, x0)
 
 @test isa(mf, Mombastic.OnceDiffMoments)
 
-@test all(Mombastic.jacobian!(mf, x0) .≈ -z'*x)
-@test all(Mombastic.momentfunction!(mf, x0) .≈ z'y)
+@test all(Mombastic.jacobian_esteq!(mf, x0, Val{Mombastic.AutoDiff}) .≈ -z'*x)
+@test all(Mombastic.jacobian_esteq!(mf, x0, Val{Mombastic.FiniteDiff}) .≈ -z'*x)
 
-@test all(Mombastic.jacobian!(mf, rand(8)) .≈ -z'*x)
-@test all(Mombastic.momentfunction!(mf, x0) .≈ z'y)
-
-
-mgr = Mombastic.TwoStepGMM()
-cs  = NLSolversBase.OnceDifferentiableConstraints([-10 for j in 1:p], [10 for j in 1:p])
-
-m = gmm(mf, x0, mgr, cs)
-
-nl = MathProgBase.NonlinearModel(Ipopt.IpoptSolver())
-
-MathProgBase.loadproblem!(nl, p, 0, getgmmbounds(m.c)..., Float64[], Float64[], :Min, m)
-
-MathProgBase.setwarmstart!(nl, x0)
-
-MathProgBase.optimize!(nl)
-
-
-
-function getgmmbounds(cs::OnceDifferentiableConstraints)
-    if NLSolversBase.nconstraints_x(cs.bounds) == 0
-        ([-Inf for j in 1:p], [Inf for j in 1:p])
-    else
-        (reshape(cs.bounds.bx, 2, 8)[1,:], reshape(cs.bounds.bx, 2, 8)[2,:])
-    end
+@test all(Mombastic.esteq!(mf, zeros(p)) .≈ z'y)
+@test begin
+    out = zeros(m)
+    Mombastic.esteq!!(out, mf, zeros(p))
+    all(out .≈ z'y)
 end
+
+
+@test begin
+    xx = randn(p)
+    objective_gmm(mf, Matrix(I, m, m), xx) == esteq!(mf, xx)'esteq!(mf, xx)
+end
+
+@test begin
+    grad_gmm = zeros(p)
+    xx = randn(p)
+    all(gradient_gmm!(grad_gmm, mf, Matrix(I, m, m), xx) .≈ -(x'z)*sum(z.*(y-x*xx), dims = 1)')
+end
+
+
+gmm(mf. x0, TwoStepGMM())
